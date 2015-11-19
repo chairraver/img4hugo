bacae5af5ad5aa55459a51366dcb2ccbeca600e1
Frank Ridderbusch <frank@ridderbusch.name>
2015-11-19 23:33:26 +0100

    Basic version works.

--- img4hugo.go
+++ img4hugo.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -12,10 +14,13 @@ import (
 var (
 	imIdentifyCmd = "identify"
 	imConvertCmd  = "convert"
-	imgsize       [4]int
+	imgsizes      []int
 )
 
 func main() {
+
+	imgsizes = []int{1024, 640, 320, 160}
+
 	var img4hugoRootCmd = &cobra.Command{
 		Use:   "img4hugo",
 		Short: "img4hugo is an application to simply embedding images into hugo content.",
@@ -25,51 +30,88 @@ func main() {
 		Use:   "resize image",
 		Short: "Resize the image to the standard set of image sizes",
 		Run: func(cmd *cobra.Command, args []string) {
-			resize(args)
+			resize(args, imgsizes)
 		},
 	}
 
-	var toHtml = &cobra.Command{
+	var tohtml = &cobra.Command{
 		Use:   "tohtml image",
 		Short: "Produce a short HTML fragment for inclusion into a hugo post",
 		Run: func(cmd *cobra.Command, args []string) {
-			toHtml(args)
+			tohtml(args)
 		},
 	}
 
 	img4hugoRootCmd.AddCommand(resizeCmd)
-	img4hugoRootCmd.AddCommand(toHtml)
+	img4hugoRootCmd.AddCommand(tohtml)
 	img4hugoRootCmd.Execute()
 }
 
-func resize(args []string) {
+func resize(args []string, imgsizes []int) {
 	for i := 0; i < len(args); i++ {
 
-		_, err := os.Stat(args[i])
+		file := args[i]
+		ext := filepath.Ext(file)
+
+		_, err := os.Stat(file)
 		if err != nil {
-			log.Fatal("file " + args[i] + " is not accessible")
+			log.Fatal("file " + file + " is not accessible")
 		}
 
-		convertCmd := exec.Command(imConvertCmd, args[i])
+		for j := 0; j < len(imgsizes); j++ {
+			sizeStr := fmt.Sprintf("%d", imgsizes[j])
+			output := "output_" + sizeStr + ext
+
+			convertCmd := exec.Command(imConvertCmd, "-resize", sizeStr, file, output)
+			err := convertCmd.Run()
+			if err != nil {
+				log.Fatal("converting " + file + " to " + output + " failed")
+			}
+
+			sizeStr, err = getImageData(output)
+			if err != nil {
+				log.Fatal("file " + output + " is not accessible")
+			}
+
+			dest := strings.TrimSuffix(file, ext)
+			dest = dest + "_" + sizeStr + ext
+
+			err = os.Rename(output, dest)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
 }
 
-func toHtml(args []string) {
+func tohtml(args []string) {
+
 	for i := 0; i < len(args); i++ {
 
-		_, err := os.Stat(args[i])
+		_, err := getImageData(args[i])
 		if err != nil {
 			log.Fatal("file " + args[i] + " is not accessible")
 		}
-
-		identifyCmd := exec.Command(imIdentifyCmd, args[i])
-
-		identifyOut, err := identifyCmd.Output()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println(string(identifyOut))
 	}
 
+}
+
+func getImageData(img string) (sizeStr string, err error) {
+
+	_, err = os.Stat(img)
+	if err != nil {
+		return
+	}
+
+	identifyCmd := exec.Command(imIdentifyCmd, img)
+
+	identifyOut, err := identifyCmd.Output()
+	if err != nil {
+		return
+	}
+
+	res := strings.Split(string(identifyOut), " ")
+	fmt.Printf("%q\n", res)
+
+	return res[2], nil
 }
